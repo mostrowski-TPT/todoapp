cb67f7d87167c6309be649b06f277d6017201cfc
Marek Ostrowski <mostrowski@transperfect.com>
2019-02-06 16:08:55 +0100

    gites

--- src/components/todos.js
+++ src/components/todos.js
@@ -24,11 +24,11 @@ updatedate(){
   
  let milisecnds = moment().diff(date, 'miliseconds');
  let left = moment.duration(milisecnds); 
- let seconds = left._data.seconds;
- let minutes = left._data.minutes;
- let hours = left._data.hours;
- let days = left._data.days;
- let timeleft = days + "days" + hours + "hours" + minutes + "minutes" + seconds + "seconds";
+ let seconds = Math.abs(left._data.seconds);
+ let minutes = Math.abs(left._data.minutes);
+ let hours = Math.abs(left._data.hours);
+ let days = Math.abs(left._data.days);
+ let timeleft = days + " days, " + hours + " hours " + minutes + " minutes " + seconds + " seconds ";
 
  
   this.setState({
@@ -47,7 +47,7 @@ componentWillUnmount(){
 render(){
 
   return(
-    <Paper style={paperstyle}><Typography>{this.props.name}<br></br>Deadline: {moment(this.props.date).format('MMMM Do YYYY, h:mm:ss a')}<br></br>Left: {this.state.timeleft}</Typography></Paper>
+    <Paper style={paperstyle}><Typography>{this.props.name}<br></br><hr></hr>Deadline: {moment(this.props.date).format('MMMM Do YYYY, h:mm:ss a')}<br></br><hr></hr>Time left: {this.state.timeleft}</Typography></Paper>
   )
 }
 }
